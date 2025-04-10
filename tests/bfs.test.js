f6a4e9322d72e2ab716e25ffad0d469e32c58c05
Vítor Moreira Rodrigues <vitorrdrgs04@gmail.com>
2025-04-10 10:25:45 -0300

    Testes unitários finalizados

---
+++ tests/bfs.test.js
@@ -0,0 +1,33 @@
+import GraphList from "../src/models/graph_list.js";
+import GraphMatrix from "../src/models/graph_matrix.js";
+import { bfs } from "../src/services/bfs.js";
+
+test("BFS traversal from node 0 in graph list", () => {
+    const edges = [
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [0, 4]
+    ];
+
+    const graph_list = new GraphList(edges, 5);
+
+    const order = bfs(graph_list, 0);
+
+    expect(order).toEqual([0, 1, 4, 2, 3]);
+});
+
+test("BFS traversal from node 0 in graph matrix", () => {
+    const edges = [
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [0, 4]
+    ];
+
+    const graph_matrix = new GraphMatrix(edges, 5);
+
+    const order = bfs(graph_matrix, 0);
+
+    expect(order).toEqual([0, 1, 4, 2, 3]);
+});
---
+++ tests/dfs.test.js
@@ -0,0 +1,33 @@
+import GraphList from "../src/models/graph_list.js";
+import GraphMatrix from "../src/models/graph_matrix.js";
+import { dfs } from "../src/services/dfs.js";
+
+test("DFS traversal from node 0 in graph list", () => {
+    const edges = [
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [0, 4]
+    ];
+
+    const graph_list = new GraphList(edges, 5);
+
+    const order = dfs(graph_list, 0);
+
+    expect(order).toEqual([0, 1, 2, 3, 4]);
+});
+
+test("DFS traversal from node 0 in graph matrix", () => {
+    const edges = [
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [0, 4]
+    ];
+
+    const graph_matrix = new GraphMatrix(edges, 5);
+
+    const order = dfs(graph_matrix, 0);
+
+    expect(order).toEqual([0, 1, 2, 3, 4]);
+});
